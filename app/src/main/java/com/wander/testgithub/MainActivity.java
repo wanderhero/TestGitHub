45329f3ee70d63cd49b56e4a94cfa0d728dc167c
wanderhero <cengjingsea@163.com>
2016-06-01 16:49:05 +0800

    test

--- app/src/main/java/com/wander/testgithub/MainActivity.java
+++ app/src/main/java/com/wander/testgithub/MainActivity.java
@@ -1,14 +1,38 @@
 package com.wander.testgithub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.img1)
+    ImageView img1;
+    @BindView(R.id.img2)
+    ImageView img2;
+    @BindView(R.id.img3)
+    ImageView img3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         System.out.println("test wander");
+    }
+
+    @OnClick({R.id.img1, R.id.img2})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.img1:
+                break;
+            case R.id.img2:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/wander/testgithub/TestJson.java
@@ -0,0 +1,20 @@
+package com.wander.testgithub;
+
+/**
+ * Created by wander on 16/5/31.
+ */
+public class TestJson {
+    /**
+     * name : wander
+     */
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
